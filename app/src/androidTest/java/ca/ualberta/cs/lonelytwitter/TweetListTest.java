a1061746cf79abbafe7d4bb3c67c038cc7225dc2
nettapper <connerdunn7399@gmail.com>
2015-09-30 18:52:57 -0600

    What I could impliment of the lab assn

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -4,6 +4,9 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by ccdunn on 9/30/15.
  */
@@ -32,13 +35,6 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         assertFalse(tweetList.hasTweet(tweet));
     }
 
-    public void testHasTweet(){
-        TweetList tweetList = new TweetList();
-        Tweet tweet = new NormalTweet("test tweet");
-        assertFalse(tweetList.hasTweet(tweet));
-        tweetList.add(tweet);
-        assertTrue(tweetList.hasTweet(tweet));
-    }
 
     public void testAdd(){
         TweetList tweetList = new TweetList();
@@ -70,5 +66,77 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testGetTweetType(){
+    }
+
+   /*
+   Lab Assn
+
+   addTweet() -- should throw an IllegalArgumentException when one tries to add a duplicate tweet
+   getTweets() -- sould return a list of tweets in chronological order
+   hasTweet() -- should return true if there is a tweet that equals() another tweet
+   removeTweet() -- should remove a tweet
+   getCount() -- should accurately count up the tweets
+   */
+
+    public void testAddTweet() {
+        TweetList tweetList = new TweetList();
+        NormalTweet t = new NormalTweet("ksdjfks");
+        tweetList.addTweet(t);
+        assertEquals(tweetList.count(), 1); // need one tweet
+
+        Boolean flag = false;
+        try {
+            tweetList.addTweet(new NormalTweet("ksdjfks"));
+        } catch (IllegalArgumentException e){
+            flag = true;
+        }
+        assertTrue(flag); // shouldn't catch an error when same tweet content is added
+
+        Boolean newflag = false;
+        try {
+            tweetList.addTweet(t);
+        } catch (IllegalArgumentException e){
+            flag = true;
+        }
+        assertTrue(newflag); // should catch an error when same obj is added
+
+    }
+
+    public void testGetTweets() {
+        TweetList tweetList = new TweetList();
+        Tweet tweet2 = new NormalTweet("test tweet", new Date());  // created first and should have smaller date
+        Tweet tweet = new NormalTweet("test tweet", new Date());
+        tweetList.addTweet(tweet);
+        tweetList.addTweet(tweet2);  // smaller date added second
+        ArrayList<Tweet> list = tweetList.getTweets();
+
+        assertTrue(list.get(0).getDate().before(list.get(1).getDate()));
+    }
+
+    public void testHasTweet(){
+        TweetList tweetList = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+        assertFalse(tweetList.hasTweet(tweet));
+        tweetList.add(tweet);
+        assertTrue(tweetList.hasTweet(tweet));
+    }
+
+    public void removeTweet(){
+        TweetList tweetList = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+        assertFalse(tweetList.hasTweet(tweet));
+        tweetList.add(tweet);
+        assertTrue(tweetList.hasTweet(tweet));
+        tweetList.removeTweet(tweet);
+        assertFalse(tweetList.hasTweet(tweet));
+    }
+
+    public void testGetCount(){
+        TweetList tweetList = new TweetList();
+        Tweet tweet2 = new NormalTweet("test tweet");
+        Tweet tweet = new NormalTweet("test tweet");
+        tweetList.addTweet(tweet);
+        tweetList.addTweet(tweet2);
+        assertEquals(tweetList.getCount(), 2);
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -16,15 +16,43 @@ public class TweetList {
       tweets.remove(tweet);
    }
 
-   public boolean hasTweet(Tweet tweet) {
-      return tweets.contains(tweet);
-   }
-
    public Tweet getTweet(int index) {
       return tweets.get(index);
    }
 
    public int count() {
       return tweets.size();
+   }
+
+   /*
+   Lab Assn
+
+   addTweet() -- should throw an IllegalArgumentException when one tries to add a duplicate tweet
+   getTweets() -- should return a list of tweets in chronological order
+   hasTweet() -- should return true if there is a tweet that equals() another tweet
+   removeTweet() -- should remove a tweet
+   getCount() -- should accurately count up the tweets
+   */
+   public void addTweet(Tweet tweet) throws IllegalArgumentException {
+      if(!this.hasTweet(tweet)){
+         tweets.add(tweet);
+      } else throw new IllegalArgumentException();
+   }
+
+   public ArrayList<Tweet> getTweets() {
+      // todo: need to impliment
+      return null;
+   }
+
+   public boolean hasTweet(Tweet tweet) {
+      return tweets.contains(tweet);
+   }
+
+   public void removeTweet(Tweet tweet){
+      this.delete(tweet);
+   }
+
+   public int getCount(){
+      return this.count();
    }
 }
